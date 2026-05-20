902bb1ed66ebbe55e44e67ce91abb1ae979eda92
yan-vikng-dev <yan@vikng.dev>
2026-05-20 12:48:59 +0700

    Fix AutoQuit cask audit issue

--- Casks/autoquit.rb
+++ Casks/autoquit.rb
@@ -2,8 +2,7 @@ cask "autoquit" do
   version "1.0.0"
   sha256 "09619e330090349c584ad25c0339fdeca212823421406752799dbdeee5f3e970"
 
-  url "https://github.com/yan-vikng-dev/AutoQuit/releases/download/v#{version}/AutoQuit-v#{version}-macos.zip",
-      verified: "github.com/yan-vikng-dev/AutoQuit/"
+  url "https://github.com/yan-vikng-dev/AutoQuit/releases/download/v#{version}/AutoQuit-v#{version}-macos.zip"
   name "AutoQuit"
   desc "Menu bar app that closes apps after their last window is gone"
   homepage "https://github.com/yan-vikng-dev/AutoQuit"
